4364643ba2388ffd9993f34f3613a6302bfe043c
Pax <panagiotis.iatrou@gmail.com>
2014-03-21 10:12:24 +0000

    first commit

---
+++ post.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Detox</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/font-awesome.min.css">
+    
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  
+    <div class="header">
+        <div class="logo">
+            <a href="./">YOURLOGO</a>
+        </div>
+        <p class="blog-description">A beautiful modern Writus theme</p>
+    </div>
+    
+    <div class="content row">
+        <div class="article col-xs-12">
+            <a class="article-title">Street Scenes</a>
+            <p>&nbsp;</p>
+            <div class="article-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit quis justo id dictum. Fusce tempor facilisis purus ac scelerisque. Aenean velit lacus, ullamcorper eu tincidunt sed, laoreet volutpat nisl. Vivamus a risus facilisis, hendrerit diam nec, auctor erat. Suspendisse aliquam eu odio sit amet vulputate. Fusce viverra cursus hendrerit. Fusce venenatis urna in nulla dictum, sed varius neque suscipit. Praesent nec sem sed dolor varius suscipit.</p>
+                
+                <p>Vivamus blandit arcu id scelerisque vestibulum. Quisque cursus venenatis lacus, a pulvinar ante hendrerit sed. Aliquam commodo augue vitae dapibus tempor. Suspendisse hendrerit ligula non diam laoreet vestibulum. Suspendisse tempor quis odio sed consectetur. Donec lacus lacus, egestas eu lorem ac, sagittis sodales neque. Fusce sit amet volutpat metus, sit amet porta arcu. Integer adipiscing feugiat ante quis bibendum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin pretium elit sit amet pretium tristique. Sed facilisis lacinia posuere. Integer non dolor metus. Etiam rutrum risus eu felis ultricies, sagittis molestie velit euismod. Suspendisse sit amet scelerisque felis.</p>
+                
+                <p>Mauris condimentum fringilla urna eget porttitor. In vel lacinia augue, vitae mollis enim. Sed a nulla euismod, congue nulla vehicula, dapibus metus. Curabitur ornare auctor lorem a rutrum. Pellentesque luctus suscipit urna. Pellentesque vel eros malesuada, tristique elit ac, tristique elit. Donec in congue nibh. Vestibulum in dictum lectus, non euismod nulla. Fusce in mauris libero.</p>
+                
+                <p>Ut adipiscing metus molestie mauris congue tristique. Vivamus tempor, lectus eu elementum fringilla, tortor quam facilisis elit, eget vestibulum ipsum augue at lorem. Suspendisse ligula mauris, rutrum et libero eget, faucibus fermentum quam. Quisque sed purus molestie, dapibus lacus at, vulputate eros. Aliquam luctus augue quis ligula aliquam imperdiet. Morbi quis elit a ligula euismod euismod. Phasellus vel arcu quis nunc vulputate ullamcorper. Nunc id orci arcu. Phasellus mattis pharetra est, sed accumsan nisl pharetra ac. Cras luctus purus sed imperdiet rhoncus.</p>
+                
+                <p>Mauris porttitor eros vel sem aliquam, sit amet malesuada elit molestie. Nulla est mi, dictum vitae sem sed, condimentum viverra metus. Suspendisse porta vitae lectus vitae viverra. Donec enim neque, mattis nec condimentum nec, laoreet id nisl. Praesent mi quam, consequat vel nibh convallis, ultrices vestibulum ipsum. Nulla semper risus est, ut sodales ipsum pellentesque nec. Nunc pellentesque fermentum felis, semper euismod leo semper id. In id risus ligula. Suspendisse pellentesque ligula sit amet consequat condimentum. Ut tincidunt, metus at gravida hendrerit, enim velit gravida arcu, sit amet laoreet nibh turpis mollis velit.</p>
+                
+                <p>Nunc ac commodo velit. Nunc auctor, urna ut malesuada pellentesque, eros quam volutpat risus, id rhoncus ipsum sapien vitae mi. Nunc mollis erat nec mauris posuere dignissim. Nullam nibh magna, molestie in diam quis, cursus imperdiet tellus. Praesent blandit sodales turpis a ultricies. Integer laoreet, diam nec posuere aliquet, lacus lectus euismod quam, non fermentum mauris dui a risus. Etiam dolor urna, placerat id suscipit et, pretium a libero. Nulla ut mauris eu ante mollis imperdiet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In sem lacus, scelerisque eu erat at, facilisis feugiat turpis. Aliquam eros urna, auctor sit amet mauris vel, varius hendrerit nulla. Morbi lobortis quam ut urna commodo, a semper tellus lacinia. Morbi sit amet dui ac risus gravida vulputate. Cras porta augue mauris, non malesuada quam dignissim laoreet. Duis vel mollis lorem, ac blandit turpis. Duis vulputate egestas dolor, eget molestie arcu interdum at.</p>
+                
+                <p>Duis vel risus scelerisque, pharetra augue et, hendrerit lorem. Aliquam bibendum tortor id ultricies vestibulum. Fusce at vehicula ipsum, ac aliquam nibh. Etiam at luctus massa. Morbi lectus erat, imperdiet eget erat ut, sagittis tincidunt elit. Mauris congue eu nulla a pulvinar. Nulla massa arcu, dignissim venenatis felis in, vulputate tempor quam. In posuere placerat diam eu elementum. Donec eu metus a felis placerat convallis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi non enim in ante ultrices convallis et eget massa.</p>
+                
+                <p>Nulla laoreet dignissim augue, vel commodo lorem iaculis vitae. In hac habitasse platea dictumst. Nunc accumsan malesuada ultricies. Proin gravida metus felis, at varius erat rutrum non. Integer lacinia elit sed lacinia tincidunt. Mauris in lacinia sapien. Etiam vulputate turpis at varius porta. Duis tempor, felis in venenatis venenatis, urna enim posuere libero, nec blandit lacus ipsum vel nisl.</p>
+                
+                <p>Integer ultrices suscipit enim, sed vestibulum leo blandit at. Vivamus consequat fermentum magna. Cras vitae nibh odio. Phasellus viverra pellentesque velit, non hendrerit orci faucibus vitae. Etiam non consectetur metus, ac condimentum ipsum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus tempor molestie quam, ut tempor ligula vestibulum vitae. Aenean commodo laoreet mollis. Nulla sem quam, aliquam non blandit ac, dignissim id tellus. Vestibulum dictum dictum turpis sed interdum. Ut non erat ut nisl consequat accumsan a eu velit. Integer mattis, tortor vehicula malesuada feugiat, diam metus dapibus tortor, vitae sollicitudin lorem risus eget felis. Sed accumsan tempor libero, at malesuada purus pharetra nec. Aliquam varius facilisis ligula, vel vehicula leo fringilla ut. Pellentesque eu aliquet eros.</p>
+                
+                <p>Morbi ornare imperdiet viverra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sagittis augue vitae erat dignissim, facilisis posuere lectus faucibus. Aenean imperdiet eget ipsum id convallis. Vestibulum id ipsum purus. Nam ut elit at velit interdum sollicitudin. Suspendisse potenti. Donec elementum, magna sit amet aliquet dignissim, nunc elit pulvinar quam, ac scelerisque odio est ut eros. Mauris posuere consectetur metus ut aliquet. Phasellus non dui quis leo ullamcorper dapibus. Curabitur sit amet consequat urna.</p>
+            </div>
+            
+            <hr>
+            
+            <div class="article-footer">
+                <div class="row">
+                    <div class="col-xs-6 text-left">WORDS BY CAL BOWMAN</div>
+                    <div class="col-xs-6 text-right">4 NOV 2013</div>
+                </div>
+                
+                <div class="row margin-top-10">
+                    <div class="col-xs-6 text-left">SHARE: <a href="#"><i class="fa fa-facebook"></i></a> <a href="#"><i class="fa fa-twitter"></i></a> <a href="#"><i class="fa fa-google-plus"></i></a></div>
+                    <div class="col-xs-6 text-right"><a href="#"><span class="glyphicon glyphicon-tags"></span>&nbsp;&nbsp;PHOTOS, URBAN LIFE</a></div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    
+    <div class="footer">
+        <p><a href="./">Home</a> | <a href="./about.php">About</a> | <a href="./archive.php">Archive</a></p>
+        <p>&copy; Powered by <a href="http://www.writus.com">Writus</a></p>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="./assets/js/jquery.fittext.js"></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    
+    <script>
+        $(document).ready(function() {
+            $(".logo").fitText();
+        });
+    </script>
+
+  </body>
+</html>
